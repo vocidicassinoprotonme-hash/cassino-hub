77fc9da697ee1b0833b956f8f00ed3d58e631689
vocidicassinoprotonme-hash <vocidicassinoproton.me@gmail.com>
2026-01-25 21:21:50 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ch-cache-v2";
+const CACHE = "ch-cache-v1";
 const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
 
 self.addEventListener("install", (e)=>{
@@ -13,4 +13,5 @@ self.addEventListener("fetch", (e)=>{
     }).catch(()=> r))
   );
 });
+
 
